279ec4a50d04f116ba562e09c39b53ede7cb0590
afterloe <lm6289511@gmail.com>
2019-03-20 18:02:24 +0800

    feat: 创建设备

--- routers/device.go
+++ routers/device.go
@@ -6,6 +6,11 @@ import (
 	"../util"
 	"net/http"
 	"strconv"
+	"time"
+	"reflect"
+	"../exceptions"
+	"database/sql"
+	"../integrate/logger"
 )
 
 /**
@@ -44,4 +49,71 @@ func getDevice(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, util.Success(reply))
+}
+
+/**
+	创建设备
+ */
+func newDevice(context gin.Context) {
+	d := &device{
+		Status: true,
+		CreateTime: time.Now().Unix(),
+	}
+	d.Uid = context.PostForm("uid")
+	d.Ssid = context.PostForm("ssid")
+	d.Pwd = context.PostForm("pwd")
+	d.Name = context.PostForm("name")
+	d.Remark = context.PostForm("remark")
+	d.Position = context.PostForm("position")
+	err := d.Check("Name", "Ssid", "Pwd") // 参数检测
+	if nil != err {
+		context.JSON(http.StatusBadRequest, util.Error(err))
+		return
+	}
+	_, err = dbConnect.WithTransaction(func(tx *sql.Tx) (interface{}, error) {
+		stmt, err := tx.Prepare("INSERT INTO device(uid, ssid, pwd, name, remark, position, status, createTime) VALUES (?,?,?,?,?,?,?,?)")
+		if nil != err {
+			return nil, &exceptions.Error{Msg: "db stmt open failed.", Code: 500}
+		}
+		result, _ := stmt.Exec(d.Uid, d.Ssid, d.Pwd, d.Name, d.Remark, d.Position, d.Status, d.CreateTime)
+		id, _ := result.LastInsertId()
+		d.Id = id
+		logger.Logger("device", "insert success")
+		return nil, nil
+	})
+	if nil != err {
+		context.JSON(http.StatusBadRequest, util.Error(err))
+		return
+	}
+	context.JSON(http.StatusOK, util.Success(d))
+}
+
+type device struct {
+	Id int64 `json:"id"`
+	Uid string `json:"uid"`
+	Ssid string `json:"ssid"`
+	Pwd string `json:"pwd"`
+	Name string `json:"name"`
+	Remark string `json:"remark"`
+	Position string `json:"position"`
+	Status bool `json:"status"`
+	CreateTime int64 `json:"createTime"`
+	ModifyTime int64 `json:"modifyTime"`
+}
+
+/**
+	参数检测
+*/
+func (this *device) Check(args ...string) error {
+	value := reflect.ValueOf(*this)
+	for _, arg := range args {
+		v := value.FieldByName(arg)
+		if !v.IsValid() {
+			break
+		}
+		if "" == v.Interface() {
+			return &exceptions.Error{Msg: "lack param " + arg, Code: 400}
+		}
+	}
+	return nil
 }
